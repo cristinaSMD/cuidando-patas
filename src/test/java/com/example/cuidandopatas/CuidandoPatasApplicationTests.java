a2792554b859cf5974faee9fb3ee6c9c8108e46f
cristinaSMD <nemesisconsuerte@gmail.com>
2025-01-25 19:51:58 +0100

    Código base con pantalla de login y consulta

---
+++ src/test/java/com/example/cuidandopatas/CuidandoPatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cuidandopatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuidandoPatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
